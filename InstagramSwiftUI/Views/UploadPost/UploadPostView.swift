bab22100b68acb06731476779d450acd403fecb0
SirAndreRusso <102429266+SirAndreRusso@users.noreply.github.com>
2022-12-08 21:02:54 +0300

    Build upload post UI

--- InstagramSwiftUI/Views/UploadPost/UploadPostView.swift
+++ InstagramSwiftUI/Views/UploadPost/UploadPostView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if postImage != nil {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .foregroundColor(.black)
+                        .clipped()
+                }
+                Text("Select a photo")
+                    .font(.title)
+            } else {
+                HStack(alignment: .top) {
+                   Image("Kenny")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                    TextField("Enter your caption...", text: $captionText)
+                }
+                .padding()
+                
+                Button {
+                    
+                } label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(5)
+                }
+                
+                Spacer()
+            }
+        }
     }
 }
 
