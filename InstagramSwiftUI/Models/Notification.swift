4630c4a63163e7d43bfcfed3905e469b66918e2e
Andrei Rusin <sir.andre.russo@gmail.com>
2023-01-05 20:39:41 +0300

    Build notification model

---
+++ InstagramSwiftUI/Models/Notification.swift
@@ -0,0 +1,37 @@
+//
+//  Notification.swift
+//  InstagramSwiftUI
+//
+//  Created by Андрей Русин on 05.01.2023.
+//
+
+import FirebaseFirestoreSwift
+import Firebase
+
+enum NotificationType: Int, Decodable {
+    case like
+    case comment
+    case follow
+    
+    var notificationMessage: String {
+        switch self {
+            
+        case .like:
+            return "liked one of your posts"
+        case .comment:
+            return "commented one of your post"
+        case .follow:
+            return "followed you"
+        }
+    }
+}
+
+struct Notification: Identifiable, Decodable {
+    @DocumentID var id: String?
+    let postId: String
+    let username: String
+    let profileImageURL: String
+    let timestamp: Timestamp
+    let type: Int
+    let uid: String
+}
